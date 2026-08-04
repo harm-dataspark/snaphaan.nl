0a4db76a17436562d7c2a320b38fe1fdb4944914
harm-dataspark <144387784+harm-dataspark@users.noreply.github.com>
2026-08-04 10:27:45 +0200

    Enable Bear Markdown syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import remarkBear from './src/plugins/remark-bear.mjs';
 
 export default defineConfig({
   site: 'https://snaphaan.nl',
   output: 'static',
+  markdown: {
+    remarkPlugins: [remarkBear],
+  },
 });

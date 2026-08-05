c0223739a8669d1fa2a2f457562b2054342cc4c9
harm-dataspark <144387784+harm-dataspark@users.noreply.github.com>
2026-08-05 16:58:00 +0200

    Gebruik harmsnaphaan.nl als publiek domein

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import remarkBear from './src/plugins/remark-bear.mjs';
 
 export default defineConfig({
-  site: 'https://snaphaan.nl',
+  site: 'https://harmsnaphaan.nl',
   output: 'static',
   markdown: {
     remarkPlugins: [remarkBear],
